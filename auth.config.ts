bacb131ce463f6f530adf503345af43be6ae0f16
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2025-01-12 19:03:30 -0300

    fix authorized callback

--- auth.config.ts
+++ auth.config.ts
@@ -10,18 +10,18 @@ export const authConfig = {
       const isLoggedIn = !!auth?.user;
 
       const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
-      // const isApiRoute = nextUrl.pathname.includes("/api");
-      // const isAuthRoute = nextUrl.pathname.includes("/auth");
+      const isApiRoute = nextUrl.pathname.includes("/api");
+      const isAuthRoute = nextUrl.pathname.includes("/auth");
 
-      // if (isApiRoute) return;
+      if (isApiRoute) return;
 
       if (isPrivateRoute && !isLoggedIn) {
         return Response.redirect(new URL("/auth/signin", nextUrl));
       }
 
-      // if (isAuthRoute && isLoggedIn) {
-      //   return Response.redirect(new URL("/", nextUrl));
-      // }
+      if (isAuthRoute && isLoggedIn) {
+        return Response.redirect(new URL("/", nextUrl));
+      }
 
       return true;
     },
